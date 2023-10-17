a2f92b5adfa793d3a45aa11bd37660f4d9052ae9
Sergeyy321 <125265794+Sergeyy321@users.noreply.github.com>
2023-10-17 23:54:39 +0200

    commit three

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,15 @@ import { Filter } from 'components/Filter/Filter';
 import { Contacts } from 'components/Contacts/Contacts';
 import { Phonebook } from 'components/Phonebook/Phonebook';
 import { ContactStyled } from './App/App.styled';
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchContacts } from 'redux/operations';
+import { useSelector } from 'react-redux';
+
 
 import { selectIsLoading, selectError, selectContacts } from 'redux/selector';
 import { Loader } from './Loader/loader';
 
 export const App = () => {
   const contacts = useSelector(selectContacts);
-const dispatch = useDispatch();
+
 const isLoading = useSelector(selectIsLoading);
 const error = useSelector(selectError);
 
